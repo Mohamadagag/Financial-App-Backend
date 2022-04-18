75eeaa8585622f73057cee93297283b7cdad5826
Mohamad Agag <mohamad.agag007@gmail.com>
2022-04-18 03:48:53 -0700

    first commit

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use JWTAuth;
+use Exception;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class JwtMiddleware extends BaseMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+<<<<<<< HEAD
+        public function handle($request, Closure $next){
+            try {
+                $user = JWTAuth::parseToken()->authenticate();
+            } catch (Exception $e) {
+                if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                    return response()->json(['status' => 'Token is Invalid']);
+                }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                    return response()->json(['status' => 'Token is Expired']);
+                }else{
+                    return response()->json(['status' => 'Authorization Token not found']);}}
+            return $next($request);}}
+                    
+=======
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['status' => 'Token is Invalid']);
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['status' => 'Token is Expired']);
+            } else {
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
+        }
+        return $next($request);
+    }
+}
+>>>>>>> 2cfbe4c9509f29e92882ecf61b3cd6574452747b
